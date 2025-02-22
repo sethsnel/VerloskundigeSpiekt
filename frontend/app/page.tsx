c4f1c380a6c7f53b2aaa4be280804dc48c21d25a
Seth Snel <sethsnel@gmail.com>
2025-02-22 17:46:10 +0100

    Updated tekst

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
     <>
       <div className={styles.main}>
         <h1 className={styles.title}>Verloskundige Spiekt</h1>
-        <p>Het digitale kookboek</p>
+        <p>Je kent het vast: van die kleine dingen die je iedere keer weer even op moet zoeken. Dat wordt een stuk makkelijker met Verloskundige Spiekt! Geen chaotische notitieboekjes meer, maar een handige website met duidelijke mobiele weergave. Maak een account aan of log in met je Google account om spiekbriefjes toe te voegen of sub-onderwerpen toe te voegen aan bestaande spiekbriefjes. Ook handig voor VIO’s als stageboekje!</p>
+        <p>Disclaimer: Verloskundige Spiekt is niet aansprakelijk voor gevolgen door het gebruik van onze spiekbriefjes.</p>
+        <p>
+          Voor vragen of opmerkingen over de inhoud: <a href="mailto:inhoud@verloskundigespiekt.nl">inhoud@verloskundigespiekt.nl</a><br />
+          Voor vragen of opmerkingen over de website functionaliteiten: <a href="mailto:development@verloskundigespiekt.nl">development@verloskundigespiekt.nl</a>\
+        </p>
       </div>
     </>
   )
