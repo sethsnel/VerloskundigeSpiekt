efe1cff9b1fbe38d83a094eb49a207a22831eed3
Seth Snel <sethsnel@gmail.com>
2025-07-29 22:04:59 +0200

    fixed reindex menu function

--- frontend/components/admin/index-menu.tsx
+++ frontend/components/admin/index-menu.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { useState } from 'react'
-import { Button } from '../button'
-import getMenuItems from '@/lib/firestore/articles/get-menu-items'
+
 import { doc, setDoc } from 'firebase/firestore'
 import { firestoreDb } from 'config/firebaseConfig'
+import revalidatePath from '@/lib/firestore/articles/revalidate'
+import { getArticles } from '@/lib/firestore/articles'
+
+import { Button } from '../button'
 
 export default function IndexMenu() {
   const [isReindexing, setIsReindexing] = useState(false)
@@ -13,7 +16,7 @@ export default function IndexMenu() {
   const indexMenu = async () => {
     setError(null)
     setIsReindexing(true)
-    const menuItems = await getMenuItems()
+    const menuItems = await getArticles()
 
     try {
       const menuArticlesRef = doc(firestoreDb, 'menu', 'articles')
@@ -24,6 +27,7 @@ export default function IndexMenu() {
           return acc
         }, {} as Record<string, string>)
       )
+      revalidatePath('/', 'layout')
     } catch (err) {
       setError('Er is een fout opgetreden bij het indexeren')
       console.error('Reindex error:', err)
